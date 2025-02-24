500b16f260f507ec62ea52a11cddcffc902030f6
komal7834 <komalsu1910@gmail.com>
2025-02-24 12:06:11 +0530

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', // Change if using a remote database
+      port: 5433, // Default PostgreSQL port
+      username: 'postgres',
+      password: 'Premp7@196',
+      database: 'library',
+      autoLoadEntities: true, // Automatically load entities
+      synchronize: true, // Set to false in production
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
