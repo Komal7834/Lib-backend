4568b428a718a676b3b921aa21bbc823b45c608d
komal7834 <komalsu1910@gmail.com>
2025-03-24 16:36:37 +0530

    chnges

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow React frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  app.enableCors();
+  
   await app.listen(3001);
 }
 
